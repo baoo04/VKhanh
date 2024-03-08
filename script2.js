9ff768b46ef1c3bce8f32bf081b4cf355be1f530
baoo04 <134896716+baoo04@users.noreply.github.com>
2024-03-08 10:50:20 +0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 // Sử dụng JavaScript để thêm nội dung của bức thư vào phần tử có id "thank-you-letter"
 document.getElementById("thankk-you-letter").innerHTML =
-    "Một lần nữa, tớ chúc cậu có một ngày 8/3 thật vui vẻ, hạnh phúc, khỏe mạnh và luôn mỉm cười nhe! Khong chúc xinh đẹp nua vì xinh sẵn roii. ";
+    "Một lần nữa, tớ chúc cậu có một ngày 8/3 thật vui vẻ, hạnh phúc, khỏe mạnh và luôn mỉm cười nhe! Khong chúc xinh đẹp nua vì xinh sẵn roii. Thêm 1 dòng nữa, tối qua đi chơi với cậu tớ lại có thêm tý hi vọng :)) he. À mà lên gg dịch chữ ở dưới đii";
 
 // Thêm hiệu ứng viết từ từ vào nội dung bức thư
 const text = document.getElementById("thank-you-letter").innerText;
