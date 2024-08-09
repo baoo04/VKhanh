6b68755e31b2c835e5f92cd70af38228928dc077
baooo04 <dbao09107@gmail.com>
2024-08-09 16:41:40 +0700

    update

--- script2.js
+++ script2.js
@@ -1,22 +1,42 @@
 // Sử dụng JavaScript để thêm nội dung của bức thư vào phần tử có id "thank-you-letter"
-document.getElementById("thankk-you-letter").innerHTML =
-    "Một lần nữa, tớ chúc cậu có một ngày 8/3 thật vui vẻ, hạnh phúc, khỏe mạnh và luôn mỉm cười nhe! Khong chúc xinh đẹp nua vì xinh sẵn roii. Thêm 1 dòng nữa, tối qua đi chơi với cậu tớ lại có thêm tý hi vọng :)) he. À mà lên gg dịch chữ ở dưới đii";
+// document.getElementById("thankk-you-letter").innerHTML =
+//   "Chúc em yêu của anh mau hết mệt ạ, anh xin lỗi vì những sai lầm khum đang có vào ngày hôm qua, anh sẽ luon để ý tới cảm xúc của cam, và làm sao cho em khong phải cảm thấy khó xử và nghĩ nhiều nhé, yêu cam nhất trên đời luon >_<, số 2 khong ai số 1, mong là tụi mình sẽ càng yeu nhiều hơn, và càng thấu hiểu nhau hơn nhé ạaa";
+// if (document.getElementById("thankk-you-letter")) {
+//   const text = document.getElementById("thank-you-letter").innerText;
+//   document.getElementById("thankk-you-letter").innerHTML = "";
+//   let index = 0;
 
-// Thêm hiệu ứng viết từ từ vào nội dung bức thư
-const text = document.getElementById("thank-you-letter").innerText;
-document.getElementById("thank-you-letter").innerHTML = "";
-let index = 0;
+//   function writeLetter() {
+//     document.getElementById("thankk-you-letter").innerHTML +=
+//       text.charAt(index);
+//     index++;
+//     if (index < text.length) {
+//       setTimeout(writeLetter, 60);
+//     }
+//   }
+// }
 
-function writeLetter() {
-    document.getElementById("thankk-you-letter").innerHTML += text.charAt(index);
-    index++;
-    if (index < text.length) {
-        setTimeout(writeLetter, 60); 
-    }
+// writeLetter();
+
+document
+  .getElementById("redirect-button")
+  .addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+
+  document.body.appendChild(heart);
+  console.log("Heart");
+
+  setTimeout(() => {
+    heart.remove();
+  }, 5000);
 }
 
-writeLetter();
-
-document.getElementById('redirect-button').addEventListener('click', function() {
-    window.location.href = 'index.html';
-});
+setInterval(createHeart, 300);
